d814194a8f9c3b72dc054a4b6a10d55645f2561a
Aadhuu <athirachristopher@gmail.com>
2026-06-25 21:22:10 +0530

    cart1

--- search/urls.py
+++ search/urls.py
@@ -15,20 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from search import views
 
-from shop import views
+app_name = 'search'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.Home.as_view(), name='home'),
-    path('', include('shop.urls')),
-    path('',include('cart.urls')),
-    path('',include('search.urls')),
+    path('search/', views.Search.as_view(), name='search'),
 
 ]
 
-from django.conf.urls.static import static
-from django.conf import settings
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- search/views.py
+++ search/views.py
@@ -1,7 +1,15 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 
+from shop.models import Product
 
 class Search(View):
     def get(self, request):
-        return render(request, 'search.html')
+            query = request.GET['q']
+            p = Product.objects.filter(Q(name__icontains=query) |
+                                           Q(description__icontains=query) |
+                                           Q(price__icontains=query))
+
+            context = {'products': p}
+            return render(request, 'search.html', context)
